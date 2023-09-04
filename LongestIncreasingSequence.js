cb425ea6233ce22b735582e8f8bee42128d5de26
Thein Naing <theinnaing0908@gmail.com>
2023-09-04 15:27:57 +0800

    Update LongestIncreasingSequence.js

--- LongestIncreasingSequence.js
+++ LongestIncreasingSequence.js
@@ -1,4 +1,15 @@
 //. This is my naive solution but it is not formatted as coderbyte.
+
+/*Longest Increasing Sequence
+Have the function LongestIncreasingSequence(arr) take the array of positive integers stored in arr and 
+return the length of the longest increasing subsequence (LIS). 
+A LIS is a subset of the original list where the numbers are in sorted order, from lowest to highest, 
+and are in increasing order. The sequence does not need to be contiguous or unique, 
+and there can be several different subsequences. For example: if arr is [4, 3, 5, 1, 6] 
+then a possible LIS is [3, 5, 6], and another is [1, 6]. For this input,  
+your program should return 3 because that is the length of the longest increasing subsequence. */
+
+
 let arr = [10, 22, 9, 33, 21, 50, 41, 60];
 let i = arr.length;
 let finalLIS = []; /*1.  LIS of full array of size n is store here ===> [Length of LIS ending with element arr[i-1],
