ba1133bb04e2aaa38aaf34a6ef1cdb0b437246f2
Thein Naing <theinnaing0908@gmail.com>
2023-09-07 14:05:16 +0800

    Update LetterCountI.js

--- LetterCountI.js
+++ LetterCountI.js
@@ -1,20 +1,21 @@
 /*Letter Count I
 Have the function LetterCountI(str) take the str parameter being passed and return the first word with the greatest number of repeated letters. 
 For example: "Today, is the greatest day ever!" should return greatest because it has 2 e's (and 2 t's) and 
-it comes before ever which also has 2 e's. If there are no words with repeating letters return -1. Words will be separated by spaces.
+it comes before ever which also has 2 e's. If there are no words with repeating letters return -1. 
+Words will be separated by spaces.
+
 Examples
 Input: "Hello apple pie"
 Output: Hello
 Input: "No words"
-Output: -1*/
+Output: -1 */
 
 
-
-
+function LetterCountI(str) { 
 
 let words = str.split(" ");  //1. convert to words by spliting str.
-   let count = 0;   // 2A. initial state of count shall be "zero".
-   let result = -1; // 2B. assuming there are no words with repeating letters.
+   let count = 0;   // 2A. assuming initial state of count shall be "zero".
+   let result = -1; // 2B. assuming there are no words with repeating letters given by challange.
 
    for(let i=0; i < words.length; i++) {//3. loop through words with index i and pass the word with index i.
      let word= words[i];
