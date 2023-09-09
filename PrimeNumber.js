bc361a2f96f94c65b532eb08a73d584f9006c1bd
Thein Naing <theinnaing0908@gmail.com>
2023-09-09 17:26:39 +0800

    Update PrimeNumber.js

--- PrimeNumber.js
+++ PrimeNumber.js
@@ -1,3 +1,10 @@
+/* Prime Time
+
+Have the function PrimeTime(num) take the num parameter being passed and return the string true if the parameter is a prime number, 
+otherwise return the string false. The range will be between 1 and 2^16. */
+
+
+
 function PrimeTime(num) { 
 
   // code goes here  
