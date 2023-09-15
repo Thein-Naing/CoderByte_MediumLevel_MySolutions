a163a9b0fb569edb2d74481b07d220d5e659eba5
Thein Naing <theinnaing0908@gmail.com>
2023-09-15 21:19:47 +0800

    Update BracketMatcher.js

--- BracketMatcher.js
+++ BracketMatcher.js
@@ -24,14 +24,19 @@ function BracketMatcher(str) {
         }
     }
 
-    /* 7. we have finished checking input string using for loop . then we will continue to check given statement */
+    /* 7. we have finished checking input string using for loop. we will continue to check given statement.
+          you can use while loop or if else statement. both method are correct.*/
     
-         if ( left === right) { /* 8 . if quantity of left and right are same, the brackets are correctly matched and return 1. */
+    //      if ( left === right) { /* 8 . if quantity of left and right are same, the brackets are correctly matched and return 1. */
+    //          return 1; 
+    //       } else { // 9. otherwise quantity of left and right are not the same.so return "zero".
+    //          return 0;
+    //    }
+
+     while ( left === right) { /* 8 . if quantity of left and right are same,the brackets are correctly matched  and return 1. */
              return 1; 
-             
-            } else { // 9. otherwise quantity of left and right are not the same.so return "zero".
+      }   // 9. otherwise quantity of left and right are not the same.so return "zero".
              return 0;
-    }
 }
         
 // keep this function call here 
