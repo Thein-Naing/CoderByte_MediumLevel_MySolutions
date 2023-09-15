8f3b8e99e1a54e0067474d5e59aaa189ec4dcb55
Thein Naing <theinnaing0908@gmail.com>
2023-09-15 21:29:14 +0800

    Update BracketMatcher.js

--- BracketMatcher.js
+++ BracketMatcher.js
@@ -18,7 +18,7 @@ function BracketMatcher(str) {
         } else if(str[i] === ")" ) {        /* 5. if the character is a closing bracket update right. increment the right quantity. */
           right++;
         }
-         if (right > left) {                /* 6. if closing or opening bracket quantity is greater than > opening or closing bracket ,
+         if (right > left) {                /* 6. if closing or opening bracket quantity is greater than > opening or closing bracket,
                                                brackets are not correctly match up and return "zero" . you can write if ( left > right ) also.*/
               return 0;
         }
