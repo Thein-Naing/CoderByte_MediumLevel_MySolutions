7749e2af661806b1d630b019131ab47336d893aa
Thein Naing <theinnaing0908@gmail.com>
2023-09-15 21:25:18 +0800

    Update BracketMatcher.js

--- BracketMatcher.js
+++ BracketMatcher.js
@@ -12,32 +12,33 @@ function BracketMatcher(str) {
        let right = 0; //2. initial quantity of closing bracket shall be "zero"
 
     for (let  i = 0; i < str.length; i++){ //3.Loop through each character in the input string
-        if (str[i] === "(" ) { /* 4.if the character is an opening bracket update left.increment the left quantity. */     
+        if (str[i] === "(" ) {             /* 4.if the character is an opening bracket update left.increment the left quantity. */     
           left++; 
 
-        } else if(str[i] === ")" ) {  /* 5. if the character is a closing bracket update right. increment the right quantity. */
+        } else if(str[i] === ")" ) {       /* 5. if the character is a closing bracket update right. increment the right quantity. */
           right++;
         }
-         if (right > left) { /* 6. if closing or opening bracket quantity is greater than > opening or closing bracket ,
-                                   brackets are not correctly match up and return "zero" . you can write if ( left > right ) also.*/
+         if (right > left) {               /* 6. if closing or opening bracket quantity is greater than > opening or closing bracket ,
+                                           brackets are not correctly match up and return "zero" . you can write if ( left > right ) also.*/
               return 0;
         }
     }
-
-    /* 7. we have finished checking input string using for loop. we will continue to check given statement.
-          you can use while loop or if else statement. both method are correct.*/
+                                          /* 7. we have finished checking input string using for loop. we will continue to check given statement.
+                                          you can use while loop or if else statement. both method are correct.*/
     
-    //      if ( left === right) { /* 8. if quantity of left and right are same, the brackets are correctly matched and return 1. */
-    //          return 1; 
-    //       } else { // 9. otherwise quantity of left and right are not the same.so return "zero".
-    //          return 0;
-    //    }
-
-     while ( left === right) { /* 8. if quantity of left and right are same,the brackets are correctly matched and return 1. */
+     while ( left === right) {            /* 8. if quantity of left and right are same,the brackets are correctly matched and return 1. */
              return 1; 
-      }   // 9. otherwise quantity of left and right are not the same.so return "zero".
+      }                                   /* 9. otherwise quantity of left and right are not the same.so return "zero". */
              return 0;
 }
         
 // keep this function call here 
 console.log(BracketMatcher(readline()));
+
+ 
+    //  if else statement for step 8 and 9.
+    //      if ( left === right) { /* 8. if quantity of left and right are same, the brackets are correctly matched and return 1. */
+    //          return 1; 
+    //       } else { // 9. otherwise quantity of left and right are not the same.so return "zero".
+    //          return 0;
+    //    }
