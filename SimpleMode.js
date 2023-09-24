f98148fa74ce95e05ecfdad2aecaf0f62fc9a43a
Thein Naing <theinnaing0908@gmail.com>
2023-09-24 10:05:13 +0800

    Update SimpleMode.js

--- SimpleMode.js
+++ SimpleMode.js
@@ -15,7 +15,7 @@ for(let i = 0; i < arr.length; i++) {          // 3. loop through the array to f
     let occurence = 0;                         // 4 . initial state of occurence in each element shall be zero.
                                                // 5. note. console.log(occurence).
     for(let j = 0; j < arr.length; j++) {
-        if( j !== i && arr[i] === arr[j]) {    /* 6. assuming  j!== i and arr[i] of outer loop was same as as arr[j] in inner loop */
+        if( j !== i && arr[i] === arr[j]) {    // 6. assuming  j!== i and arr[i] of outer loop was same as as arr[j] in inner loop 
             occurence++;                       /* 7. meaning : there will be a mode as element of arr[i] from outer loop appears element of arr[j] 
                                                      in inner loop. so we update the occurence, occurence += 1.  // note. console.log(occurence). */      
         }
@@ -24,7 +24,7 @@ for(let i = 0; i < arr.length; i++) {          // 3. loop through the array to f
                                                               
             mode = arr[i]; 
             maxOccurence = occurence;          /* 9.  then occurence of arr[i] will be maximum occurence. so element of arr[i] will be mode and 
-                                                       assuming not only arr[i] is appered first in outer loop 
+             mode = arr[i];                                          assuming not only arr[i] is appered first in outer loop 
                                                        but also there is more than one mode and other mode which has same maximum occurence as arr[i] . */
                                                /* 10.  then as given statement in challenge, if there is more than one mode, 
                                                        return the one that appeared in the array first.so we return arr[i] as mode.*/
@@ -33,10 +33,9 @@ for(let i = 0; i < arr.length; i++) {          // 3. loop through the array to f
 
 }
     
-if( maxOccurence === 0) {                      // 11. if there is no multiple occurences of element, means there is no mode and return -1.
-    return -1;
-    }
-
+if( maxOccurence === 0) {                       // 11. if there is no multiple occurences of element, means there is no mode and return -1.
+    return -1; 
+}  
 return mode;                                   // 12. otherwise return mode.
 }
   
