61598c78d0b3d8aca5d3562352bdf1f3b690687a
Thein Naing <theinnaing0908@gmail.com>
2023-09-28 15:59:07 +0800

    Update ThreeFiveMultiples.js

--- ThreeFiveMultiples.js
+++ ThreeFiveMultiples.js
@@ -2,7 +2,7 @@
 
 Have the function ThreeFiveMultiples(num) return the sum of all the multiples of 3 and 5 that are below num. 
 For example: if num is 10, the multiples of 3 and 5 that are below 10 are 3, 5, 6, and 9, and adding them up you get 23, so your program should return 23. 
-The integer being passed will be between 1 and 100.
+The integer being passed will be between 1 and 100. 
 
 Examples
 Input: 6
@@ -11,7 +11,7 @@ Input: 1
 Output: 0
 
 Tags
-searching math fundamentals  * /
+searching math fundamentals  */
 
 
 function ThreeFiveMultiples(num) {
