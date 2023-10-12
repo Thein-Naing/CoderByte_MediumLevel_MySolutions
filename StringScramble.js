9b109e433a28fff826f91495fd498812680023ac
Thein Naing <theinnaing0908@gmail.com>
2023-10-12 13:37:04 +0800

    Update StringScramble.js

--- StringScramble.js
+++ StringScramble.js
@@ -11,10 +11,10 @@ for (let i = 0; i < str2.length; i++) {           // 1. loop through str2 using 
     
                                                   /* 2. Assume each character of str2 equal to a portion of str1 characters.
                                                         example: str1.indexOf(0) === str2.CharAt(0) or
-                                                        str1.indexOf(1) === str2.CharAt(2) or
-                                                        str1.indexOf(2) === str2.CharAt(1) or
-                                                        str1.indexOf(3) === str2.CharAt(4) or
-                                                        str1.indexOf(4) === str2.CharAt(3) e.t.c. */
+                                                                 str1.indexOf(1) === str2.CharAt(2) or
+                                                                 str1.indexOf(2) === str2.CharAt(1) or
+                                                                 str1.indexOf(3) === str2.CharAt(4) or
+                                                                 str1.indexOf(4) === str2.CharAt(3) e.t.c. */
 
     if (str1.indexOf(str2.charAt(i)) !== -1) {   // 3. if a portion of str1 characters were found in str2 characters.(indexOf functions is to return -1 when the character isn't found.)
      str1 = str1.replace(str2[i], "");           // 4. then str1 will be replaced with rearranged str2 characters using str.replace method..
