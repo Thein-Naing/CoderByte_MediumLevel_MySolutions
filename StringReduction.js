20ec89fe25d654d2f7e29d9447e74f0a8e16a6cc
Thein Naing <theinnaing0908@gmail.com>
2023-10-24 18:44:30 +0800

    Update StringReduction.js

--- StringReduction.js
+++ StringReduction.js
@@ -16,12 +16,27 @@ Output: 4
 Tags
 string manipulation recursion */
 
+/* Given Statement: The method is: Only the letters a, b, and c will be given in str and you must take two different adjacent characters and replace it with the third.                   
+                    1. So abc can be replaced with two different adjacent characters of a , b and c which are ab,ba,ac,ca,bc,cb.                     
+                    
+   Given Statement: This method is done repeatedly until the string cannot be further reduced, and the length of the resulting string is to be outputted.
+                    2. We will use regex.test() method with while loop.
+                      (i) The while loop loops through a block of code as long as a specified condition is true. 
+                          Here the specified condition is  /(ab|ba|ac|ca|bc|cb)/g
+                          
+                      (ii) The regex.test(string) method is used to test for a match in a string. The method returns true if it finds a match; otherwise, it returns false.
+                     So regex  === /(ab|ba|ac|ca|bc|cb)/g  and regex.test(string) === /(ab|ba|ac|ca|bc|cb)/g.test(str). Meaning: pattern "ab","ba", etc... matched with given string? etc.
+                     
+                      (iii) If /(ab|ba|ac|ca|bc|cb)/g.test(str) finds a match, then replace them with str.replace(/ab|ba/, "c").replace(/ca|ac/, "b").replace(/bc|cb/, "a") 
+                            using str.replace() method and given reduction method.
+                    3. Return the smallest number using str.length() method. */
+
 
 function StringReduction(str) { 
 
   // code goes here  
     while (/(ab|ba|ac|ca|bc|cb)/g.test(str)) {
-   str = str.replace(/ab|ba/, "c").replace(/ca|ac/, "b").replace(/bc|cb/, "a"); 
+        str = str.replace(/ab|ba/, "c").replace(/ca|ac/, "b").replace(/bc|cb/, "a"); 
    }
    return str.length;
 }
