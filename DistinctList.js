f69781587cbfbfcb6f7e2fdfc774c3c5f9e497f2
Thein Naing <theinnaing0908@gmail.com>
2023-10-25 18:28:16 +0800

    Create DistinctList.js

---
+++ DistinctList.js
@@ -0,0 +1,23 @@
+/* medium
+Distinct List
+Have the function DistinctList(arr) take the array of numbers stored in arr and determine the total number of duplicate entries.
+For example if the input is [1, 2, 2, 2, 3] then your program should output 2 because there are two duplicates of one of the elements.
+
+Examples
+Input: [0,-2,-2,5,5,5]
+Output: 3
+Input: [100,2,101,4]
+Output: 0
+
+Tags
+array math fundamentals set theory sorting */
+
+function DistinctList(arr) { 
+
+  // code goes here  
+  return arr.filter((num, index) => arr.indexOf(num) !== index).length;      /* 1. Use arr.filter() method together with arr.indexOf() method and arr.length method and return it.
+}                                                                                  if arr.indexOf(0) ===  0  !== [0,-2,-2,5,5,5][0], then there will be duplicate entries and
+                                                                                   filter this element and find its length.   */
+   
+// keep this function call here 
+console.log(DistinctList(readline()));
