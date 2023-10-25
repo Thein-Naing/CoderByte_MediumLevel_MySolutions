a9e9316c6cd3735fc9cbdbddd1172b1699e36292
Thein Naing <theinnaing0908@gmail.com>
2023-10-25 18:42:03 +0800

    Update DistinctList.js

--- DistinctList.js
+++ DistinctList.js
@@ -10,14 +10,19 @@ Input: [100,2,101,4]
 Output: 0
 
 Tags
-array math fundamentals set theory sorting */
+array math fundamentals set theory sorting */ 
+
+ /* Note: The arr.indexOf() method returns the index of the first occurrence the element that we want to find, or -1 if the element is not found.
+          The filter() Array method creates a new array with elements that fall under a given criteria from an existing array. */
 
 function DistinctList(arr) { 
 
   // code goes here  
-  return arr.filter((num, index) => arr.indexOf(num) !== index).length;      /* 1. Use arr.filter() method together with arr.indexOf() method and arr.length method and return it.
-}                                                                                  if arr.indexOf(0) ===  0  !== [0,-2,-2,5,5,5][0], then there will be duplicate entries and
-                                                                                   filter this element and find its length.   */
-   
+  return arr.filter((num, index) => arr.indexOf(num) !== index).length;      /* 1. Use arr.filter() method(num and index as arguments) together with arr.indexOf() method and arr.length method.
+}                                                                                  If arr.indexOf(0) ===  0  !== [0,-2,-2,5,5,5][0], then there will be duplicate entries.
+                                                                                   Filter this element and find its length and return it. */
+   ts
 // keep this function call here 
 console.log(DistinctList(readline()));
+
+
