c2177bc02f1e6ec921d4098e7c16b8c8c3728d59
Thein Naing <theinnaing0908@gmail.com>
2023-10-25 13:40:38 +0800

    Update NumberEncoding.js

--- NumberEncoding.js
+++ NumberEncoding.js
@@ -21,9 +21,9 @@ function NumberEncoding(str) {
   for (let i = 0; i < str.length; i++) {      // 2. Use For Loop to loop through each every character to check if it is a letter or symbol or white space.
     if (!/[a-z]/.test(str[i])) {              /* 3. Use regex.test() method to verify character of given str[i] is a letter. 
                                                      Meaning: !/[a-z]/.test(str[i])  ===  there is no letter found in given str[i].  */ 
-        stack.push(str[i]);                   // 4.If it is not a letter, it may be symbol or white space and add to stack.  
+        stack.push(str[i]);                   // 4.If it is not a letter, it may be symbol or white space and add to stack using arr.push() method.  
         } else {
-        stack.push(str.charCodeAt(i) - 96);   /* 5.If it is a letter, then use charCodeAt() method to convert letter to its char code.
+        stack.push(str.charCodeAt(i) - 96);   /* 5.If it is a letter, add to stack using arr.push() method and use charCodeAt() method to convert letter to its char code.
                                                              Note: The charCodeAt() method returns the numeric Unicode value of the character at the given index.
                                                    The char code for lowercase is 97 and so to get 1-indexed alphabet position for str[i], substract 96 from 97(str.charCodeAt(i)) */
         }
