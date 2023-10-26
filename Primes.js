1607c11649fc7ca9217bdca8a7f233c96e7f1db5
Thein Naing <theinnaing0908@gmail.com>
2023-10-26 16:06:18 +0800

    Update Primes.js

--- Primes.js
+++ Primes.js
@@ -6,31 +6,21 @@ The range will be between 1 and 2^16. */
  // Note. same solution as PrimeNumber.
 
 function Primes(num) { 
-
   // code goes here  
- // 1. initial state of prime number shall be false.
-let isPrimes = false;
 
-/* 2. take note 
-2A."1 is neither prime nor composite number."
-2B. prime number not be is less than 1
-2C. prime number must be positive value.
+/* Note: 1. "1 is neither prime nor composite number."
+         2. prime number not be is less than 1
+         3. prime number must be positive value. */
 
-//3. so we will start from num > 1  and loop through starting from 2. 
- and we will check if remainder === 0 then it is not prime number */
 
- if (num > 1) {  
-    for (let i = 2; i < num; i++) {
-        if (num % i === 0) {
-          return  isPrimes = false;
-        }
+for (let i = 2;  i < num; i++)    /*4. so we will loop through starting from 2 and we will check if remainder === 0 then it is divisible and it is not prime number */
+    if (num % i === 0) {       
+      return "false";
     }
-
-//4.  then return isPrime with ternary operator for other numbers.
- return  isPrimes ? false : true;
-         
   }
-}
+  return "true";                  // 5. Otherwise it is not divisible and it is prime number
+      
+ }
    
 // keep this function call here 
 console.log(Primes(readline()));
