8712e1c90bdc74b279b335521298aed4ae2207c0
Thein Naing <theinnaing0908@gmail.com>
2023-10-31 17:25:54 +0800

    Update PalindromicSubstring.js

--- PalindromicSubstring.js
+++ PalindromicSubstring.js
@@ -13,6 +13,12 @@ Output: none
 Tags
 searching dynamic programming Google Microsoft */
 
+/* Solution Notes:  1. It will be Nested Loop for each letter in the input string and palindromic substrings.
+                    2. Looping through expanding to the left(for given str[i]) and right(for longest substring str[j]) while checking for even and odd length palindromic substrings. 
+        	    3. Move to the next letter if a palindromic substring doesn’t exist.
+                    4. By expanding one character to the left and right, compare them and if both of them are equal , there is a palindromic substring.
+		    5. Add this palindrome to stack and continue to find another palindromic substring.
+                    6. Sort the stack, check longest palindromic substring is greater than 2, then return it otherwise return 0. */
 
 function PalindromicSubstring(str) { 
 
