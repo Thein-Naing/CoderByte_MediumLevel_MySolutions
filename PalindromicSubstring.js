1b996a5f85b9ce1aece57c3b551acc21d46bc783
Thein Naing <theinnaing0908@gmail.com>
2023-10-31 17:03:48 +0800

    Create PalindromicSubstring.js

---
+++ PalindromicSubstring.js
@@ -0,0 +1,32 @@
+/* Palindromic Substring
+Have the function PalindromicSubstring(str) take the str parameter being passed and find the longest palindromic substring, which means the longest substring which is read the same forwards as it is backwards. 
+For example: if str is "abracecars" then your program should return the string racecar because it is the longest palindrome within the input string.
+The input will only contain lowercase alphabetic characters. 
+The longest palindromic substring will always be unique, but if there is none that is longer than 2 characters, return the string none.
+
+Examples
+Input: "hellosannasmith"
+Output: sannas
+Input: "abcdefgg"
+Output: none
+
+Tags
+searching dynamic programming Google Microsoft */
+
+
+function PalindromicSubstring(str) { 
+
+  // code goes here  
+let  stack=[];
+	for(let i = 0; i < str.length; i++) {
+		for(let j = i+1; j < str.length;j++) {
+			if(str.slice(i-1, j+1) === str.slice(i-1, j+1).split('').reverse().join('')) {
+				stack.push(str.slice(i-1, j+1))
+			}
+		}
+	}
+	return stack.sort((a,b) => b.length - a.length)[0].length > 2 ? stack[0]:'none'
+}
+   
+// keep this function call here 
+console.log(PalindromicSubstring(readline()));
